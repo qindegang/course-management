497842f8cf0b1fdd86747544bf2d339506e81869
qindegang <qindegang@gmail.com>
2024-09-21 21:01:14 +0800

    Implement the sign-up feature

---
+++ src/test/java/ai/minden/course_management/repository/CourseRepositoryTest.java
@@ -0,0 +1,44 @@
+package ai.minden.course_management.repository;
+
+import ai.minden.course_management.entity.Course;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class CourseRepositoryTest {
+
+    private static final String COURSE_NAME = "english";
+    private static final Course COURSE = new Course(COURSE_NAME);
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @BeforeEach
+    public void setUp() {
+        courseRepository.save(COURSE);
+    }
+
+    @Test
+    public void findByName_success() {
+        Optional<Course> course = courseRepository.findByName(COURSE_NAME);
+        assertThat(course).contains(COURSE);
+    }
+
+    @Test
+    public void findByName_not_found() {
+        Optional<Course> course = courseRepository.findByName("abc");
+        assertThat(course).isEmpty();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        courseRepository.delete(COURSE);
+    }
+}
---
+++ src/test/java/ai/minden/course_management/repository/StudentRepositoryTest.java
@@ -0,0 +1,49 @@
+package ai.minden.course_management.repository;
+
+import ai.minden.course_management.entity.Student;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class StudentRepositoryTest {
+
+    private static final String ALICE = "alice@a.com";
+
+    private Student studentAlice;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @BeforeEach
+    public void setUp() {
+        studentAlice = new Student(ALICE);
+        studentRepository.save(studentAlice);
+    }
+
+    @Test
+    public void findByEmail_success() {
+        Optional<Student> student = studentRepository.findByEmail(ALICE);
+        assertThat(student).contains(studentAlice);
+    }
+
+    @Test
+    public void findByEmail_not_found() {
+        Optional<Student> student = studentRepository.findByEmail("x@a.com");
+        assertThat(student).isEmpty();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        studentRepository.delete(studentAlice);
+    }
+}
